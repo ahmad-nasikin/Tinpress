67da0e6b70e04367479553111673c4a27f886e9d
Ahmad Nasikin <ahmad.nasikin09@gmail.com>
2017-09-20 17:55:53 +0700

    skeleton

---
+++ server/app.js
@@ -0,0 +1,24 @@
+
+const express = require('express');
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const logger = require('morgan');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/tinpres');
+
+// const index = require('./routes/index');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(cors())
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// app.use('/', index)
+
+
+app.listen(3000, () => {
+  console.log('Listening Port 3000');
+})
