9290ac13b37140f21a84642f28c06c2b342765d5
Ahmad Nasikin <ahmad.nasikin09@gmail.com>
2017-09-21 21:38:04 +0700

    logout tp main url masih bisa ditembak

--- server/models/User.js
+++ server/models/User.js
@@ -1,9 +1,9 @@
 var mongoose = require('mongoose');
-
 var Schema = mongoose.Schema;
 
 var userSchema = new Schema({
   username: String,
+  email: String,
   password: String,
   fbId: String
 });
