f5afb622fbdfde203e1890e958b992924c8cf3d1
Yosua1011 <yosua1011@gmail.com>
2017-09-21 07:31:27 +0700

    comparison between face API and emotion API

--- facetest.js
+++ facetest.js
@@ -1,16 +1,33 @@
-function closest(list, x) {
+function closest(list,x) {
+    // var min,
+    //     chosen = list[0];
+    // for (var i in list) {
+    //     min = Math.abs(chosen - x);
+    //     if (Math.abs(list[i] - x) < min) {
+    //         chosen = list[i];
+
+    //     }
+    // }
+    // var arr = []
+    // for (var i in list) {
+        
+    // }
+
     var min,
-        chosen = list[0];
+    chosen = list[0];
     for (var i in list) {
         min = Math.abs(chosen - x);
         if (Math.abs(list[i] - x) < min) {
             chosen = list[i];
-
         }
     }
 
-    return list.indexOf(chosen);
+    console.log(list.indexOf(chosen))
+    console.log(list.indexOf(list[list.indexOf(chosen)+1]))
+    console.log(list.indexOf(list[list.indexOf(chosen)+2]))
+    // console.log(list.indexOf(chosen))
 }
 
 let list = [0,0,0,0,0,0.986,0.009,0.005]
-console.log(closest(list,1))
+// list.sort(function(a,b){return b-a})
+console.log(closest(list,3))
