97014a96d10f551f97f0eea27f5077b8cd31f16d
rahid <hidayat.85@gmail.com>
2017-09-22 11:03:21 +0700

    create model Image, change db location to atlas

---
+++ server/controllers/imagesController.js
@@ -0,0 +1,26 @@
+const Image = require ('../models/Image');
+
+
+const createImage = function (req, res) {
+  Image.create({
+    imgURL: req.body.imgurl,
+  })
+  .then(() => {
+    res.send({msg:`tambah gambar berhasil`})
+  })
+  .catch(err => {
+    return res.status(400).send({msg: err.message})
+  })
+}
+
+const getImages = function (req, res) {
+  Image.find({})
+  .then(images => res.send(images))
+  .catch(err => res.send({msg: err.message}))
+}
+
+
+module.exports = {
+  createImage,
+  getImages
+}
